7dbdc8d5b541ae01896a9ed1beb6fe68f4086318
Daniel Gordon <xkcd@cs.washington.edu>
2019-09-25 16:56:26 -0700

    removed git check

--- hw1/submit.sh
+++ hw1/submit.sh
@@ -4,9 +4,6 @@ rm -rf submit/
 mkdir -p submit
 
 prepare () {
-    if [[ $(git diff origin -- $1 | wc -c) -eq 0 ]]; then
-        echo "WARNING: $1 is unchanged according to git."
-    fi
     cp $1 submit/
 }
 
