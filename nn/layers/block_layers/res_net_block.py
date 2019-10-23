a974122f271a91a36cf9dc726e3ed778b9ea1349
Daniel Gordon <xkcd@cs.washington.edu>
2019-10-23 13:00:22 -0700

    whoops...

--- nn/layers/block_layers/res_net_block.py
+++ nn/layers/block_layers/res_net_block.py
@@ -4,7 +4,7 @@ from .. import *
 class ResNetBlock(LayerUsingLayer):
     def __init__(self, conv_params, parent=None):
         super(ResNetBlock, self).__init__(parent)
-        self.conv_layers = SequentialLayer([ConvLayer(*conv_params), ReLULayer(), ConvLayer(*conv_params)], parent)
+        self.conv_layers = SequentialLayer([ConvLayer(*conv_params), ReLULayer(), ConvLayer(*conv_params)], self.parent)
         self.add_layer = None
         self.relu2 = None
 
